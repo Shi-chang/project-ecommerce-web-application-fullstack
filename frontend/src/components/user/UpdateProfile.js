ca6dd6fe750437dd16d616fcb1c40f561c0ce90e
Shi-chang <shichangye@outlook.com>
2022-09-02 21:19:51 -0700

    Change the layout and button styles of user profile page

--- frontend/src/components/user/UpdateProfile.js
+++ frontend/src/components/user/UpdateProfile.js
@@ -6,18 +6,19 @@ import { loadUser, updateProfile } from '../../actions/userActions.js';
 import { clearUserUpdateErrors, resetUpdateProfile } from '../../reducers/userUpdateSlice.js';
 
 const UpdateProfile = () => {
-    const [name, setName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { user } = useSelector(state => state.user);
     const { loading, isUpdated, error } = useSelector(state => state.userUpdate);
 
-    // Once the registration process is complete and the user is authenticated, navigate to the
-    // home page.
+    // Once the update process is complete, navigate to the /me home page.
     useEffect(() => {
         if (user) {
-            setName(user.name);
+            setFirstName(user.name.split(' ')[0]);
+            setLastName(user.name.split(' ')[1]);
             setEmail(user.email);
         }
         if (isUpdated) {
@@ -34,6 +35,7 @@ const UpdateProfile = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        let name = `${firstName} ${lastName}`;
         const formData = { name, email }
         dispatch(updateProfile(formData));
     }
@@ -44,16 +46,27 @@ const UpdateProfile = () => {
             <div className="row wrapper">
                 <div className="col-10 col-lg-5">
                     <form className="shadow-lg" onSubmit={handleSubmit} encType='multipart/form-data'>
-                        <h1 className="mt-2 mb-5">Update Profile</h1>
+                        <h2 className="mt-2 mb-5 text-center">Update Profile</h2>
                         <div className="form-group">
-                            <label htmlFor="name-field">Name</label>
+                            <label htmlFor="name-field1">First Name</label>
                             <input
-                                type="name"
-                                id="name-field"
+                                type="text"
+                                id="name-field1"
                                 className="form-control mb-3"
-                                name='name'
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
+                                name='firstName'
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name-field2">Last Name</label>
+                            <input
+                                type="text"
+                                id="name-field2"
+                                className="form-control mb-3"
+                                name='lastName'
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -67,10 +80,15 @@ const UpdateProfile = () => {
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
-                        <button
-                            type="submit"
-                            className="btn update-btn btn-block mt-4 mb-3"
-                            disabled={loading ? true : false} >Update</button>
+                        <div className='d-flex justify-content-center'>
+                            <button
+                                type="submit"
+                                className="btn"
+                                id='primary-button'
+                                disabled={loading ? true : false} >
+                                Update
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
