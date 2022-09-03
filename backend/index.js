4389107f50f6514f5f45741290476742eb1f459a
Shi-chang <shichangye@outlook.com>
2022-09-02 21:10:26 -0700

    Set up configuration for cloudinary

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
 import connectDatabase from './config/database.js';
 
 // Sets up .env path.
@@ -7,6 +8,14 @@ dotenv.config({ path: 'backend/config/config.env' })
 
 // connects to the database.
 connectDatabase();
+
+// Sets up configuration for cloudinary.
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
 
 // Listens on the specified port.
 const server = app.listen(process.env.PORT, () => {
