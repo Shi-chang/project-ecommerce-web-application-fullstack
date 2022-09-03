0b0e3dadf0caae99fdb54bf4c6d0372d5396a3c5
Shi-chang <shichangye@outlook.com>
2022-09-02 21:04:17 -0700

    Update size limit for the uploaded image

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import cloudinary from 'cloudinary';
 import productsRoutes from './routes/product.js';
 import userRoutes from './routes/user.js';
 import orderRoutes from './routes/order.js';
@@ -20,21 +19,14 @@ const app = express();
 // Sets up .env path.
 dotenv.config({ path: 'backend/config/config.env' })
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ limite: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
-
-// Sets up configuration for cloudinary.
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 // Middleware that handles different routes. The products and user routes are mostly accomplished both
 // in the front end and back end. But the order fuction is not finished yet in the front end.
