e96baa7de87cf34d4f5b9015f22b71f3dc6b8a43
Shi-chang <shichangye@outlook.com>
2022-09-02 21:22:07 -0700

    Change the login button in the header component

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Search from './Search.js';
