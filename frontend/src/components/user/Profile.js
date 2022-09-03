d8798e8f2e34bc6487c677db8b3e1342f6d33dfb
Shi-chang <shichangye@outlook.com>
2022-09-02 21:17:02 -0700

    Update profile page layout and buttons

--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -16,31 +16,43 @@ const Profile = () => {
                     <MetaData title="My Profile" />
                     <div className="row justify-content-around mt-5 user-info">
                         <div className="col-12 col-md-3">
-                            <h2 className="mt-5 ml-5">My Profile</h2>
-                            <figure className='avatar avatar-profile'>
-                                <img className="rounded-circle img-fluid" src={user.avatar.url} alt='user avatar' />
-                            </figure>
-                            <Link to='/me/update' id="edit-profile" className="btn btn-primary my-5">
-                                Edit profile
-                            </Link>
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <h2>My Profile</h2>
+                            </div>
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <figure className='avatar avatar-profile'>
+                                    <img className="rounded-circle img-fluid" src={user.avatar.url} alt='user avatar' />
+                                </figure>
+                            </div>
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <Link to='/me/update' id="edit-profile" className="btn btn-dark my-5">
+                                    Edit profile
+                                </Link>
+                            </div>
                         </div>
-                        <div className="col-12 col-md-5">
-                            <h4>Name</h4>
-                            <p>{user.name}</p>
-                            <h4>Email</h4>
-                            <p>{user.email}</p>
-                            <h4>Account created at</h4>
-                            <p>{String(user.createdAt).substring(0, 10)}</p>
-                            <Link to='/orders/me' href="#" className="btn btn-success btn-block mt-3 me-3">
-                                My orders
-                            </Link>
-                            <Link to='' href="/password/update" className="btn btn-primary btn-block mt-3">
-                                Change my password
-                            </Link>
+                        <div className="col-12 col-md-4 mb-0">
+                            <div>
+                                <h4>Name</h4>
+                                <p>{user.name}</p>
+                                <h4>Email</h4>
+                                <p>{user.email}</p>
+                                <h4>Account created at</h4>
+                                <p>{String(user.createdAt).substring(0, 10)}</p>
+
+                                <button className="btn me-3" id='primary-button'>
+                                    <Link to='/orders/me' style={{ textDecoration: 'none', color: 'black' }}> My orders </Link>
+                                </button>
+                                <button className="btn btn-danger">
+                                    <Link to='/password/update' style={{ textDecoration: 'none', color: 'white' }}>
+                                        Change password
+                                    </Link>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </>
-            )}
+            )
+            }
         </>
     )
 }
