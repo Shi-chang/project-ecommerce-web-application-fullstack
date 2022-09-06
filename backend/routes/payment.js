da54c4679cfe14bfbcabc25d11571317e9c1c39f
Shi-chang <shichangye@outlook.com>
2022-09-06 08:58:30 -0700

    Add payment.js

---
+++ backend/routes/payment.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { processPayment, sendStripeApiKey } from '../controller/paymentController.js';
+import { isAuthenticatedUser } from "../middlewares/authentication.js";
+
+const router = express.Router();
+
+router.route('/payment/process').post(isAuthenticatedUser, processPayment);
+router.route('/stripeapikey').get(isAuthenticatedUser, sendStripeApiKey);
+
+export default router;
