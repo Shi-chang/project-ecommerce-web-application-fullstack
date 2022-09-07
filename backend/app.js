91738453a7cdd280f2ee96bbcd7ec31093162e76
Shi-chang <shichangye@outlook.com>
2022-09-06 23:23:09 -0700

    Adjust json parser and body parser for stripe webhook route

--- backend/app.js
+++ backend/app.js
@@ -5,36 +5,53 @@ import cookieParser from 'cookie-parser';
 import productsRoutes from './routes/product.js';
 import userRoutes from './routes/user.js';
 import orderRoutes from './routes/order.js';
-import paymentRoute from './routes/payment.js';
+// import paymentRoute from './routes/payment.js';
+import stripeRoute from './routes/stripe.js';
 import errorMiddleWare from './middlewares/errorMiddleware.js';
 import fileUpload from 'express-fileupload';
 import dotenv from 'dotenv';
 import path from 'path';
-
 import { fileURLToPath } from 'url';
+
+// Sets up .env path.
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Sets up .env path.
-dotenv.config({ path: 'backend/config/config.env' })
-
-app.use(express.json({ limit: '10mb' }));
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
-app.use(bodyParser.urlencoded({ limite: '10mb', extended: true }));
+
+console.log(`${process.env.BACKEND_URL}/stripe/webhook`);
+
+// Uses JSON parser and body parser for all non-webhook routes. The reason for this is that the 
+// webhook endpoint (in stripe.js) needs to take the raw request to do signature check. Modifying 
+// the request before the check would normally result in a stripe error - "Wehook error. No signatures
+// found matching the expected signature for payload."
+
+// https://github.com/stripe/stripe-node/blob/master/examples/webhook-signing/node-express/express.js
+app.use((req, res, next) => {
+    if (req.originalUrl === `/stripe/webhook`) {
+        next();
+    } else {
+        express.json({ limit: '10mb' })(req, res, next);
+        bodyParser.urlencoded({ limite: '10mb', extended: true });
+    }
+});
+
 app.use(cookieParser());
 app.use(fileUpload());
 
-// Middleware that handles different routes. The products and user routes are mostly accomplished both
-// in the front end and back end. But the order fuction is not finished yet in the front end.
+// Middleware that handles different routes.
 app.use(productsRoutes);
 app.use(userRoutes);
-app.use(paymentRoute);
+// app.use(paymentRoute);
 app.use(orderRoutes);
+app.use("/stripe", stripeRoute);
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
@@ -44,7 +61,8 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
     })
 }
 
-// middleware that handles errors
+// Middleware that handles errors based on the mode of the project. The errorMiddleware helps generate
+// an informative error message and sends the message back to the frontend in the production mode.
 app.use(errorMiddleWare);
 
 export default app;
