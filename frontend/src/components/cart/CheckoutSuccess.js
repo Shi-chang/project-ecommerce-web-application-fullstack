eb471b5cf40d35be6f56967119e87bf1bb18bb36
Shi-chang <shichangye@outlook.com>
2022-09-06 23:40:58 -0700

    Add CheckoutSuccess.js

---
+++ frontend/src/components/cart/CheckoutSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CheckoutSuccess = () => {
+    return (
+        <div>
+            <h2>
+                CheckoutSuccess
+            </h2>
+        </div>
+    )
+}
+
+export default CheckoutSuccess;
