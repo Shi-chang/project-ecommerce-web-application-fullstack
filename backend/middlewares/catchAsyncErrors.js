3b00aafe3a9bc2b972648aadae167456d4f224ee
Shi-chang <shichangye@outlook.com>
2022-09-08 08:54:32 -0700

    Change catchAsyncErrors to catchAsyncError

--- backend/middlewares/catchAsyncErrors.js
+++
@@ -1,7 +0,0 @@
-// This function component is a middleware that catches async errors. The catched error, 
-// if any, will be passed to the next() function and will be passed to express eventually.
-const catchAsyncError = (func) => (req, res, next) => {
-    Promise.resolve(func(req, res, next)).catch(next);
-}
-
-export default catchAsyncError;
