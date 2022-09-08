d564fae71841fc8a13868fcc05f42e10e0a790a5
Shi-chang <shichangye@outlook.com>
2022-09-08 09:02:19 -0700

    Replace original button with Payment component

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -3,42 +3,34 @@ import { Link } from 'react-router-dom';
 import MetaData from '../layout/MetaData.js';
 import { addItemToCart, removeItemFromCart } from '../../actions/cartActions.js';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import PayButton from './PayButton.js';
+import Payment from './Payment.js';
 
 // The Cart component that handles CRUD operations of the shopping cart.
 const Cart = () => {
     const dispatch = useDispatch();
-    const navigate = useNavigate();
-
     const { cartItems } = useSelector(state => state.cart);
 
-    const increaseQuantity = (id, quantity, stock) => {
+    const increaseQuantity = (productId, quantity, stock) => {
         const newQuantity = quantity + 1;
         if (newQuantity > stock) {
             return;
         }
 
-        dispatch(addItemToCart(id, newQuantity));
+        dispatch(addItemToCart(productId, newQuantity));
     }
 
-    const decreaseQuantity = (id, quantity) => {
+    const decreaseQuantity = (productId, quantity) => {
         const newQuantity = quantity - 1;
         if (newQuantity < 1) {
             return;
         }
 
-        dispatch(addItemToCart(id, newQuantity));
+        dispatch(addItemToCart(productId, newQuantity));
     }
 
-    const handleRemoveCartItem = (id) => {
-        dispatch(removeItemFromCart(id));
+    const handleRemoveCartItem = (productId) => {
+        dispatch(removeItemFromCart(productId));
     }
-
-    const handleCheckout = () => {
-        navigate('/shipping');
-    }
-
 
     return (
         <div className='cart-wrapper container'>
@@ -50,14 +42,14 @@ const Cart = () => {
                     <div className="row justify-content-center">
                         <div className="col-12 col-md-9">
                             {cartItems.map(item => (
-                                <div key={item.product} className="cart-item">
+                                <div key={item.productId} className="cart-item">
                                     <div className="row">
                                         <div className="col-4 col-lg-3">
                                             <img src={item.image} alt={item.name} height="90" width="115" />
                                         </div>
 
                                         <div className="col-5 col-lg-3">
-                                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                            <Link to={`/product/${item.productId}`}>{item.name}</Link>
                                         </div>
                                         <div className="col-4 col-lg-2 mt-4 mt-lg-0">
                                             <p id="card_item_price">{item.price}</p>
@@ -65,14 +57,14 @@ const Cart = () => {
 
                                         <div className="col-4 col-lg-3 mt-4 mt-lg-0">
                                             <div className="stockCounter d-inline">
-                                                <span className="btn btn-danger minus" onClick={() => decreaseQuantity(item.product, item.quantity, item.stock)}>-</span>
+                                                <span className="btn btn-danger minus" onClick={() => decreaseQuantity(item.productId, item.quantity, item.stock)}>-</span>
                                                 <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
 
-                                                <span className="btn btn-primary plus" onClick={() => increaseQuantity(item.product, item.quantity)}>+</span>
+                                                <span className="btn btn-primary plus" onClick={() => increaseQuantity(item.productId, item.quantity)}>+</span>
                                             </div>
                                         </div>
                                         <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                                            <i id="delete_cart_item" className="fa fa-trash btn btn-danger" onClick={() => handleRemoveCartItem(item.product)}></i>
+                                            <i id="delete_cart_item" className="fa fa-trash btn btn-danger" onClick={() => handleRemoveCartItem(item.productId)}></i>
                                         </div>
 
                                     </div>
@@ -89,8 +81,7 @@ const Cart = () => {
                                 <p>Est. total: <span className="order-summary-values">${cartItems.reduce((accumulator, current) => (accumulator + current.price * current.quantity), 0).toFixed(2)}</span></p>
                                 <hr />
 
-                                {/* <button id="checkout-btn" className="btn btn-block" onClick={handleCheckout}>Check out</button> */}
-                                <PayButton cartItems={cartItems} />
+                                <Payment />
                             </div>
                         </div>
                     </div>
