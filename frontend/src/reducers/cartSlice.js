c44c6addc3b9cf8c26416da9752516b016ff7902
Shi-chang <shichangye@outlook.com>
2022-09-08 09:05:43 -0700

    Remove saveShippingInfo action type

--- frontend/src/reducers/cartSlice.js
+++ frontend/src/reducers/cartSlice.js
@@ -10,22 +10,19 @@ export const cartSlice = createSlice({
     reducers: {
         addToCart: (state, action) => {
             const item = action.payload;
-            const itemExists = state.cartItems.find(e => e.product === item.product);
+            const itemExists = state.cartItems.find(e => e.productId === item.productId);
             if (itemExists) {
-                state.cartItems = state.cartItems.map(i => i.product === itemExists.product ? item : i)
+                state.cartItems = state.cartItems.map(i => i.productId === itemExists.productId ? item : i)
             } else {
                 state.cartItems = [...state.cartItems, item];
             }
         },
         removeFromCart: (state, action) => {
-            state.cartItems = state.cartItems.filter(i => i.product !== action.payload);
-        },
-        saveShippingInfo: (state, action) => {
-            state.shippingInfo = action.payload;
+            state.cartItems = state.cartItems.filter(i => i.productId !== action.payload);
         }
     },
 })
 
-export const { addToCart, removeFromCart, saveShippingInfo } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
