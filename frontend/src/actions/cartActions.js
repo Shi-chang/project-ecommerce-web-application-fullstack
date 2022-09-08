94ee9256efa4464ed8efd25c8f03621e476ba73a
Shi-chang <shichangye@outlook.com>
2022-09-08 09:01:32 -0700

    Remove saveShippingInfo component

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { addToCart, removeFromCart, saveShippingInfo } from '../reducers/cartSlice.js';
+import { addToCart, removeFromCart } from '../reducers/cartSlice.js';
 import PORT from '../components/route/routeConstants.js';
 
 // Adds an item to the cart.
-export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
-    const url = `${PORT}/product/${id}`;
+export const addItemToCart = (productId, quantity) => async (dispatch, getState) => {
+    const url = `${PORT}/product/${productId}`;
     const { data } = await axios.get(url);
 
     dispatch(addToCart({
-        product: data.product._id,
+        productId: data.product._id,
         name: data.product.name,
         price: data.product.price,
         stock: data.product.stock,
@@ -20,15 +20,8 @@ export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
 }
 
 // Removes an item from the cart.
-export const removeItemFromCart = (id) => async (dispatch, getState) => {
-    dispatch(removeFromCart(id));
+export const removeItemFromCart = (productId) => async (dispatch, getState) => {
+    dispatch(removeFromCart(productId));
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
-}
-
-// Saves an user's shipping information.
-export const saveShippingInformation = (shippingData) => async (dispatch) => {
-    dispatch(saveShippingInfo(shippingData));
-
-    localStorage.setItem('shippingInfo', JSON.stringify(shippingData));
 }
