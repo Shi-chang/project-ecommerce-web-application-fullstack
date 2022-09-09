1cd09d0d59ad375405b7db09df831af290c02cf8
Shi-chang <shichangye@outlook.com>
2022-09-08 22:13:54 -0700

    Update CheckoutSuccess.js

--- frontend/src/components/cart/CheckoutSuccess.js
+++ frontend/src/components/cart/CheckoutSuccess.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MetaData from '../layout/MetaData.js';
+
 
 const CheckoutSuccess = () => {
     return (
-        <div>
-            <h2>
-                CheckoutSuccess
-            </h2>
-        </div>
+        <>
+            <MetaData title="Order Succeeds" />
+
+            <div className="row justify-content-center">
+                <div className="col-6 mt-5 text-center">
+                    <img className="my-5 img-fluid d-block mx-auto" src="/images/order-succeeds.png" alt="order succeeds" width="200" height="200" />
+
+                    <h3>Order placed successfully! Thank you!</h3>
+
+                    <Link to="/orders/me">Check my orders?</Link>
+                </div>
+
+            </div>
+        </>
     )
 }
 
