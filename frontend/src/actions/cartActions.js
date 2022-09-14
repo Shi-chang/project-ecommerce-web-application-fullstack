fef0a0c17e0a0724329aa1aefaebff9d740d962c
Shi-chang <shichangye@outlook.com>
2022-09-13 22:56:56 -0700

    Update the handleCartChange function

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import { addToCart, removeFromCart } from '../reducers/cartSlice.js';
+import { addToCart, removeFromCart } from '../reducers/userInfoSlice.js';
 import PORT from '../components/route/routeConstants.js';
+import store from '../store.js';
 
 // Adds an item to the cart.
 export const addItemToCart = (productId, quantity) => async (dispatch, getState) => {
-    const url = `${PORT}/product/${productId}`;
-    const { data } = await axios.get(url);
+    const productUrl = `${PORT}/product/${productId}`;
+    const { data } = await axios.get(productUrl);
 
     dispatch(addToCart({
         productId: data.product._id,
@@ -16,12 +17,31 @@ export const addItemToCart = (productId, quantity) => async (dispatch, getState)
         quantity
     }));
 
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+    await handleCartChange();
 }
 
 // Removes an item from the cart.
 export const removeItemFromCart = (productId) => async (dispatch, getState) => {
     dispatch(removeFromCart(productId));
 
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+    await handleCartChange();
+}
+
+// Handles the cart change.
+const handleCartChange = async () => {
+    const { userInfo } = store.getState();
+    const cartItems = userInfo.user.cartItems;
+
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        }
+        const updateCartUrl = `${PORT}/cart/update`;
+        await axios.put(updateCartUrl, cartItems, config);
+    } catch (error) {
+        alert(error.response.data.message);
+    }
 }
