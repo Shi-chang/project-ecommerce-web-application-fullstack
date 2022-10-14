4cf9253799db67b8102e788ba2162a5c3d374c04
Shi-chang <shichangye@outlook.com>
2022-10-13 21:47:16 -0700

    Update comment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-// Connects to the database and log out message.
+// Connects to the database and logs out message.
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_URI).then(res => {
         console.log(`${res.connection.host} is connected to Mongodb database successfully.`);
